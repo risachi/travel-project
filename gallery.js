42924953f31fe6867b07e2a2fdba1881d5402b63
Randallonius <jonathanprandall@gmail.com>
2015-12-07 14:25:26 -0800

    took out old js that I left in by mistake

--- gallery.js
+++ gallery.js
@@ -1,38 +1,3 @@
-/*var galleryImages = new Array()
-galleryImages[0] = new Image()
-galleryImages[0].src = "images/mtHoodLake.jpg"
-galleryImages[1] = new Image()
-galleryImages[1].src = "images/multnomah.jpg"
-galleryImages[2] = new Image()
-galleryImages[2].src = "images/haystack.jpg"
-galleryImages[3] = new Image()
-galleryImages[3].src = "images/portlandHill.jpg"
-galleryImages[4] = new Image()
-galleryImages[4].src = "images/portlandDusk.jpg"
-galleryImages[5] = new Image()
-galleryImages[5].src = "images/portlandHood.jpg"
-galleryImages[6] = new Image()
-galleryImages[6].src = "images/portlandTheater.jpg"
-galleryImages[7] = new Image()
-galleryImages[7].src = "images/timberline.jpg"
-galleryImages[8] = new Image()
-galleryImages[8].src = "images/whitestag.jpg"
-
-//variable that will increment through the images
-var next=0;
-
-function slide()
-{
- document.getElementById('images').src = galleryImages[next].src;
- if (next<2)
-  next++;
- else
-  next=0;
- //call function "slideit()" every 2.5 seconds
- setTimeout("slide()",2500);
-}
-slide();
-*/
 window.onload = function () {
 
     var slides = [
