ebf3d4443765b4c9843aabf1b38a5d647afce76d
Jacob S Hillman <jacob.s.hillman@gmail.com>
2015-12-14 19:52:03 -0800

    contactus: init

--- contactus.js
+++ contactus.js
@@ -1,9 +1,30 @@
-window.addEventListener("load", function ()
-  var parent = document.getElementById("content");
-  var button = parent.createChildElement("input");
-    button.type = "button";
-    button.value = "Contact Us";
+var contactType = ["Email", "Phone", "Meeting"];
+
+var form = document.createElement("form");
+  form.id = "contactForm";
+var parent = document.getElementById("content");
+var button = document.createElement("input");
+  button.type = "button";
+  button.value = "Contact Us";
+  button.name = "contactus";
+  button.id = "contactus";
+  button.addEventListener("click", function () {
+    contactFields();
+  })
+parent.appendChild(button);
 
 
 
-  )
+function contactFields() {
+  parent.appendChild(form);
+  for (index = 0; index < contactType; index++) {
+    var method = document.createElement("input");
+      method.type = "radio";
+      method.name = "contactMe";
+      method.id = index;
+      method.value = contactType[index];
+      form.appendChild("method");
+
+  } /*for loop closure. */
+}; /*function contactFields closure.  */
+
