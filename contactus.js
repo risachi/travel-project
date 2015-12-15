7065e7cd72e144cb2e31cc699cc686494121be1f
Jacob S Hillman <jacob.s.hillman@gmail.com>
2015-12-14 19:08:41 -0800

    contactus: init

---
+++ contactus.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function ()
+  var parent = document.getElementById("content");
+  var button = parent.createChildElement("input");
+    button.type = "button";
+    button.value = "Contact Us";
+
+
+
+  )
