e65c3667d5906a18c5f7576d2f4b68d21f81dd64
Danielle H <daheber@gmail.com>
2015-12-16 20:51:17 -0800

    persistence works

--- activities.js
+++ activities.js
@@ -59,6 +59,7 @@ function updateResults(){
   }
   resultsCheck();
   savedChoices = choices;
+  localStorage.setItem("savedChoices", JSON.stringify(savedChoices));
 }
 
 /***When no results are found for that search - throw an error message***/
@@ -70,11 +71,6 @@ function resultsCheck(){
 }
 
 /***Persistence stuff***/
-window.addEventListener("beforeunload", storeChoices);
-
-function storeChoices(event) {
-  localStorage.setItem("savedChoices", JSON.stringify(savedChoices));
-}
 
 window.addEventListener("load", getChoices);
 
@@ -85,8 +81,80 @@ function getChoices(event){
     }
     else {
       savedChoices = localChoices;
-      console.log(savedChoices);
+      //console.log(savedChoices);
+      loadChoices(savedChoices);
     }
+}
+
+function loadChoices(savedChoices){
+  /***Clear out the results area***/
+  restultsDiv.innerHTML = " ";
+
+  /***Object to store the values chosen***/
+  var choices = savedChoices;
+
+  if(choices.isIndoor == false || choices.isIndoor == true ){
+    if(choices.isIndoor == false){
+      document.getElementById("outdoor").checked = true;
+    }
+    else {
+      document.getElementById("indoor").checked = true;
+    }
+  }
+
+  if(choices.intensity){
+    if(choices.intensity == "low"){
+      document.getElementById("lowIntensity").checked = true;
+    }
+    else if(choices.intensity == "medium"){
+      document.getElementById("mediumIntensity").checked = true;
+    }
+    else {
+      document.getElementById("highIntensity").checked = true;
+    }
+  }
+
+  if(choices.cost){
+    if(choices.cost == "lowCost"){
+      document.getElementById("lowCost").checked = true;
+    }
+    else if(choices.cost == "medCost"){
+      document.getElementById("medCost").checked = true;
+    }
+    else {
+      document.getElementById("highCost").checked = true;
+    }
+  }
+
+  if(choices.kidFriendly == false || choices.kidFriendly == true){
+    if(choices.kidFriendly == false){
+      document.getElementById("noKids").checked = true;
+    }
+    else {
+      document.getElementById("kids").checked = true;
+    }
+  }
+
+  /***Console logging out the current choices***/
+  var keys = Object.keys(choices);
+  for(var index=0; index<keys.length; index++){
+    //console.log(keys[index] + ": " + choices[keys[index]]);
+  }
+
+  for(var index=0; index<allActivities.length; index++){
+    var showActivity = true;
+    for(var counter=0; counter<keys.length; counter++){
+     var property = keys[counter];
+     showActivity = showActivity && (allActivities[index][property] == choices[property]);
+    }
+    if(showActivity){
+      //console.log("match "+allActivities[index].name);
+      restultsDiv.innerHTML += "<p>"+allActivities[index].name+"</p>";
+    }
+  }
+  resultsCheck();
+  savedChoices = choices;
+  localStorage.setItem("savedChoices", JSON.stringify(savedChoices));
 }
 
 
